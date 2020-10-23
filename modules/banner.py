99853862caeb1ad9e535844fc440b1e32f76054c
Psyrens <73334651+Psyrens@users.noreply.github.com>
2020-10-23 18:36:25 +0500

    Add files via upload

---
+++ modules/banner.py
@@ -0,0 +1,35 @@
+from colorama import Fore, Style
+
+def banner():
+
+    Logo ="""
+                   {0}00{1}{2}                   
+                   {0}11{1}{2}                   
+                  {0}===={1}{2}               
+                  .{0}//{1}{2}                                                 
+                 `o{0}//{1}{2}:                           {3}BlackAWZ{4}            
+                `+o{0}//{1}{2}o:             {3}#--------------v0.1---------------#{4}
+               `+oo{0}//{1}{2}oo:            {3}| KERNIX3 |**************| FOX1EN |{4}
+               -+oo{0}//{1}{2}oo+:           {3}|             PSYRENS             |{4}
+             `/:-:+{0}//{1}{2}ooo+:          {3}|_________________________________|{4}    
+            `/+++++{0}//{1}{2}+++++:         {3}|    PHP   *  PYTHON   *  JS      |{4}
+           `/++++++{0}//{1}{2}++++++:        {3}|_________________________________|{4}
+          `/+++oooo{0}//{1}{2}ooooooo/`            {3}|++++++|--help|++++++|{4}
+         ./ooosssso{0}//{1}{2}osssssso+`           {3}|++++++|--tool|++++++|{4}
+        .oossssso-`{0}//{1}{2}`/ossssss+`          {3}|_______PSYRENS______|{4}
+       -osssssso.  {0}//{1}{2}  :ssssssso.            
+      :osssssss/   {0}//{1}{2}   osssso+++.         {3}1{4} ) Start tool
+     /ossssssss/   {0}//{1}{2}   +ssssooo/-         2 {3}){4} vpn
+   `/ossssso+/:-   {0}//{1}{2}   -:/+osssso+-       {3}3{4} ) update
+  `+sso+:-`        {0}//{1}{2}       `.-/+oso:
+ `++:.             {0}//{1}{2}           `-/+/                           
+ .`                {0}/{1}{2}                `/ 
+ """.format(Fore.CYAN, Style.RESET_ALL, Style.BRIGHT+Fore.RED, Fore.WHITE, Fore.RED, Style.RESET_ALL)
+    Logo = Style.BRIGHT+Fore.RED+Logo
+    print(Logo)
+
+
+
+#__KerNix3__#
+#2020-BlackAWZ-tool-coding#
+#all-right-for-PSYRENS-team#
